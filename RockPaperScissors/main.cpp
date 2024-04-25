153fa119477d5180008a95db2a92deb33be952fb
AlexD36 <alexduta36@gmail.com>
2024-04-26 00:17:06 +0300

    Updated main.cpp
    -Made the game endless
    -Added scoring system

--- RockPaperScissors/main.cpp
+++ RockPaperScissors/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cstdlib>
 #include <ctime>
 
@@ -11,51 +11,69 @@ int generareMiscareCalculator() {
 
 // Functie pentru a afisa optiunile disponibile
 void afisareOptiuni() {
+    cout << "0. Iesi\n";
     cout << "1. Piatra\n";
     cout << "2. Foarfeca\n";
     cout << "3. Hartie\n";
 }
 
-// Functie pentru a determina rezultatul jocului
-void determinaCastigatorul(int miscareJucator, int miscareCalculator) {
-    if (miscareJucator == miscareCalculator) {
+// Functie pentru a determina rezultatul jocului si a actualiza scorul
+void determinaCastigatorul(int miscareJucator, int miscareCalculator, int& scorJucator, int& scorCalculator) {
+    if (miscareJucator == 0) {
+        cout << "Iesire din joc.\n";
+    }
+    else if (miscareJucator == miscareCalculator) {
         cout << "Egalitate!\n";
     }
     else if ((miscareJucator == 1 && miscareCalculator == 2) ||
         (miscareJucator == 2 && miscareCalculator == 3) ||
         (miscareJucator == 3 && miscareCalculator == 1)) {
         cout << "Ai castigat!\n";
+        scorJucator++;
     }
     else {
         cout << "Calculatorul a castigat!\n";
+        scorCalculator++;
     }
 }
 
 int main() {
     int miscareJucator;
     int miscareCalculator;
+    int scorJucator = 0;
+    int scorCalculator = 0;
 
     // Initializare pentru generarea de numere aleatorii
     srand(time(0));
 
     cout << "Bine ati venit la jocul Piatra, Foarfeca, Hartie!\n";
-    cout << "Alege o miscare:\n";
-    afisareOptiuni();
-    cin >> miscareJucator;
 
-    // Verificare validitate miscare jucator
-    while (miscareJucator < 1 || miscareJucator > 3) {
-        cout << "Miscare invalida! Te rog alege o miscare valida:\n";
+    do {
+        cout << "Alege o miscare (0 pentru iesire):\n";
         afisareOptiuni();
         cin >> miscareJucator;
-    }
 
-    miscareCalculator = generareMiscareCalculator();
-    cout << "Calculatorul a ales: " << miscareCalculator << endl;
+        // Verificare validitate miscare jucator
+        while (miscareJucator < 0 || miscareJucator > 3) {
+            cout << "Miscare invalida! Te rog alege o miscare valida:\n";
+            afisareOptiuni();
+            cin >> miscareJucator;
+        }
 
-    cout << "Jucatorul a ales: " << miscareJucator << endl;
+        if (miscareJucator != 0) {
+            miscareCalculator = generareMiscareCalculator();
+            cout << "Calculatorul a ales: " << miscareCalculator << endl;
+            cout << "Jucatorul a ales: " << miscareJucator << endl;
 
-    determinaCastigatorul(miscareJucator, miscareCalculator);
+            determinaCastigatorul(miscareJucator, miscareCalculator, scorJucator, scorCalculator);
+
+            cout << "Scorul curent:\n";
+            cout << "Jucator: " << scorJucator << " | Calculator: " << scorCalculator << endl;
+        }
+    } while (miscareJucator != 0);
+
+    cout << "Multumim pentru joc! Scor final:\n";
+    cout << "Jucator: " << scorJucator << " | Calculator: " << scorCalculator << endl;
 
     return 0;
 }
